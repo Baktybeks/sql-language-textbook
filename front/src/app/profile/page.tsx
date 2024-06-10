bf291db6ee7091e9e8c31628b1a1550cbba23c6a
Viktor <viktor4strlsov@gmail.com>
2024-06-10 21:07:10 +0600

    amin

--- front/src/app/profile/page.tsx
+++ front/src/app/profile/page.tsx
@@ -1,15 +1,22 @@
-import styles from '../styles/profile/Profile.module.scss'
-import {useSession} from "next-auth/react";
+'use client'
 
-export default async function Profile() {
+import {useSession} from "next-auth/react";
+import styles from '../styles/profile/Profile.module.scss'
+import Layout from "@/components/layout/Layout";
+
+const Profile = () => {
   const session = useSession();
 
   return (
+      <Layout>
       <div className={styles.wrapperProfile}>
           {session?.data?.user?.image &&
-              <img className={styles.imgProfile} src={session?.data?.user.image} alt="image"/>
+              <img className={styles.imgProfile} src={session?.data?.user?.image} alt="image"/>
           }
           <h1 className={styles.nameProfile}>{session?.data?.user?.name}</h1>
       </div>
+      </Layout>
   )
 }
+
+export default Profile
