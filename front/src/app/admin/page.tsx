d19b23f90ecd30198a4fa7febd9352ff50ab28cd
Viktor <viktor4strlsov@gmail.com>
2024-06-26 02:18:13 +0600

    main

--- front/src/app/admin/page.tsx
+++ front/src/app/admin/page.tsx
@@ -17,7 +17,7 @@ interface Direction {
 
 const PageAdmin = () => {
     const [books, setBooks] = useState<Direction[]>([]);
-    const [gender, setGender] = useState<any[]>([]);
+    const [training, setTraining] = useState<any[]>([]);
     const [newBooks, setNewBooks] = useState<Direction>({
         id: '',
         title: '',
@@ -27,8 +27,12 @@ const PageAdmin = () => {
         publication_year: '',
         image: '',
     });
-
-    console.log(books)
+    const [newTraining, setNewTraining] = useState<any>({
+        id: '',
+        title: '',
+        description: '',
+        image: '',
+    });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -50,11 +54,26 @@ const PageAdmin = () => {
                 throw new Error('Unable to fetch posts!');
             }
             const jsonData = await response.json();
-            setGender(jsonData);
+            setTraining(jsonData);
         };
 
         fetchData();
     }, []);
+
+    const handleChangeTraining = (e: any) => {
+        const { name, value } = e.target;
+        if (name === 'image') {
+            setNewTraining((prevState: any) => ({
+                ...prevState,
+                [name]: e.target.files[0]
+            }));
+        } else {
+            setNewTraining((prevState: any) => ({
+                ...prevState,
+                [name]: value
+            }));
+        }
+    };
 
     const handleChange = (e: any) => {
         const { name, value } = e.target;
@@ -86,6 +105,23 @@ const PageAdmin = () => {
             console.error('Ошибка при выполнении запроса:', error);
         }
     };
+
+    const handleDeleteTraining = async (index: string) => {
+        try {
+            const response = await fetch(`http://localhost:5000/api/training-block/${index}`, {
+                method: 'DELETE'
+            });
+            if (response.ok) {
+                setTraining((book: any) => book.filter((app: any) => app.id !== index));
+                console.log('Объект удален')
+            } else {
+                console.error('Ошибка при удалении направления:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Ошибка при выполнении запроса:', error);
+        }
+    };
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
@@ -110,6 +146,37 @@ const PageAdmin = () => {
                 }
                 const jsonData = await res.json();
                 setBooks(jsonData);
+
+                console.log('добавлен объект');
+            } else {
+                console.error('Ошибка при добавлении нового направления:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Ошибка при выполнении запроса:', error);
+        }
+    };
+
+    const handleSubmitTraining = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const formData = new FormData();
+            formData.append('id', newTraining.id);
+            formData.append('title', newTraining.title);
+            formData.append('description', newTraining.description);
+            formData.append('image', newTraining.image);
+
+            const response = await fetch('http://localhost:5000/api/training-block/', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                const res = await fetch('http://localhost:5000/api/training-block/');
+                if (!res.ok) {
+                    throw new Error('Unable to fetch directions!');
+                }
+                const jsonData = await res.json();
+                setTraining(jsonData);
 
                 console.log('добавлен объект');
             } else {
@@ -161,6 +228,31 @@ const PageAdmin = () => {
                             <button className={styles.summit} type="submit">Отправить</button>
                         </form>
                     </div>
+                    <div>
+                        <h2 className={styles.nameAdmin}>Добавить новую услугу</h2>
+                        <form className={styles.formAdmin} onSubmit={handleSubmitTraining}>
+                            <div className={styles.inputForm}>
+                                <label>Название:</label>
+                                <input className={styles.input} placeholder='Название' type="text" name="title"
+                                       value={newTraining.title} onChange={handleChangeTraining}/>
+                            </div>
+                           <div className={styles.inputForm}>
+                            <label>Текст:</label>
+                            <input className={styles.input} placeholder='Текст' type="text" name="description"
+                                   value={newTraining.description} onChange={handleChangeTraining}/>
+                        </div>
+                            <div className={styles.inputForm}>
+                                <label>Картинка:</label>
+                                <div className={styles.blockImages}>
+                                    <input className={styles.imagesInput} type="file" name="image"
+                                           accept='/image/*, .png, .jpg, .web'
+                                           onChange={handleChangeTraining}/>
+                                </div>
+                            </div>
+
+                            <button className={styles.summit} type="submit">Отправить</button>
+                        </form>
+                    </div>
                 </div>
 
                 <h2 className={styles.nameBooksList}>Добавленные Книги</h2>
@@ -179,6 +271,24 @@ const PageAdmin = () => {
                             </li>
                         ))
                    }
+                </ul>
+
+                <h2 className={styles.nameBooksList}>Добавленные услуг</h2>
+                <ul className={styles.blockList}>
+                    {training.map((elem) => (
+                        <li key={elem.id} className={styles.infoList}>
+                            <div>
+                                <img src={`http://localhost:5000/${elem.image}`} alt='tower'
+                                     className={styles.imgesBooks}/>
+                                <div className={styles.textBooks}>
+                                    <div className={styles.renovationBook}>{elem.title}</div>
+                                    <div className={styles.prise}>{elem.description}</div>
+                                </div>
+                            </div>
+                            <button className={styles.delete} onClick={() => handleDeleteTraining(elem.id)}>Удалить</button>
+                        </li>
+                    ))
+                    }
                 </ul>
             </div>
         </Layout>
